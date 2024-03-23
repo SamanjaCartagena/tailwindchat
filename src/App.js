b91d3b9b6c4541e6a3f1105343a111238c3fbe4b
Samanja Cartagena <c.samanja09@Pabs.local>
2024-03-23 13:33:54 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import React from 'react'
+const style ={
+  appContainer:`max-w-[728px] mx-auto text-center`
+}
+function App() {
+  return (
+    <div className={style.appContainer}>
+    <section className='flex flex-col h[90vh] bg-gray-100 mt-10 shadow-xl border relative'>
+
+    </section>
+    </div>
+  );
+}
+
+export default App;
